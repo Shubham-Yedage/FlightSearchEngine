b7add5f4ece0a38eaec859be93fbe3b03af98060
Rahul Shukla <rahul.shukla@synerzip.com>
2016-04-04 21:04:18 +0530

    FeedBack

--- src/com/FlightSearchEngineClient.java
+++ src/com/FlightSearchEngineClient.java
@@ -5,37 +5,35 @@ import java.util.List;
 import java.util.Scanner;
 
 public class FlightSearchEngineClient {
-	private static SearchFlight sf;
+    private static SearchFlight sf;
 
-	public static void main(String[] args) {
-		String[] paths = new String[] {
-				"/home/synerzip/workspace/FlightSearchEngine/csv_Files/AIR_FRANCE.csv",
-				"/home/synerzip/workspace/FlightSearchEngine/csv_Files/BRITISH_AIRWAYS.csv",
-				"/home/synerzip/workspace/FlightSearchEngine/csv_Files/LUFTHANSA_AIRWAYS.csv",
-				"/home/synerzip/workspace/FlightSearchEngine/AirIndia.csv"};
-		sf = new SearchFlight(paths);
-		scanDetails();
-	}
+    public static void main(String[] args) {
+        String[] paths = new String[]{
+                "/home/synerzip/workspace/FlightSearchEngine/csv_Files/AIR_FRANCE.csv",
+                "/home/synerzip/workspace/FlightSearchEngine/csv_Files/BRITISH_AIRWAYS.csv",
+                "/home/synerzip/workspace/FlightSearchEngine/csv_Files/LUFTHANSA_AIRWAYS.csv",
+                "/home/synerzip/workspace/FlightSearchEngine/AirIndia.csv"};
+        sf = new SearchFlight(paths);
+        scanDetails();
+    }
 
-	public static void scanDetails() {
-		
-		List<String> finalList=new ArrayList<String>(); 
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Departure Location:");
-		String depLoc = sc.next();
-		System.out.println("Enter Arrival Location:");
-		String arrLoc = sc.next();
-		System.out.println("Enter Flight Date:");
-		String flightDate = sc.next();
-		System.out
-				.println("Enter\n 1 To sort According To Fare\n 2 To sort According To Duration");
-		int choice = sc.nextInt();
-		finalList=sf.getFlights(depLoc, arrLoc, flightDate, choice);
-		sc.close();
-		 for(Object obj:finalList)
-		 {
-			 System.out.println(""+obj);
-		 }
+    public static void scanDetails() {
 
-	}
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Departure Location:");
+        String depLoc = sc.next();
+        System.out.println("Enter Arrival Location:");
+        String arrLoc = sc.next();
+        System.out.println("Enter Flight Date:");
+        String flightDate = sc.next();
+        System.out
+                .println("Enter\n 1 To sort According To Fare\n 2 To sort According To Duration");
+        int choice = sc.nextInt();
+        List<String> finalList = sf.getFlights(depLoc, arrLoc, flightDate, choice);
+        sc.close();
+        for (String obj : finalList) {
+            System.out.println(obj);
+        }
+
+    }
 }
--- src/com/SearchFlight.java
+++ src/com/SearchFlight.java
@@ -12,125 +12,117 @@ import java.util.Map;
 import java.util.Set;
 
 public class SearchFlight {
-	protected ArrayList<String> listPath = new ArrayList<String>();
-	protected List<String> finalList=new ArrayList<String>();
-	public SearchFlight(String[] path) {
+    public static final String STRING_COMMA = ",";
+    protected ArrayList<String> listPath = new ArrayList<String>();
+    protected List<String> finalList = new ArrayList<String>();
 
-		for (String p : path) {
-			listPath.add(p);
-		}
+    public SearchFlight(String[] path) {
 
-	}
+        for (String p : path) {
+            listPath.add(p);
+        }
 
-	public String toString(String string, String string2, String string3,
-			String string4, String string5, String string6, String string7) {
-		return "|" + string + "|" + string2 + "|" + string3 + "|" + string4
-				+ "|" + string5 + "|" + string6 + "|" + string7 + "|";
-	}
+    }
 
-	public List<String> getFlights(String depLoc, String arrLoc, String flightDate,
-			int choice) {
+    public String toString(String string, String string2, String string3,
+                           String string4, String string5, String string6, String string7) {
+        return "|" + string + "|" + string2 + "|" + string3 + "|" + string4
+                + "|" + string5 + "|" + string6 + "|" + string7 + "|";
+    }
 
-		String newLine = "";
-		BufferedReader br = null;
-		String splitBy = ",";
-		int flag=0;
-		int selCol=5;
-		
-		if (choice == 1)
-			selCol = 6;
-		else
-			selCol = 5;
+    public List<String> getFlights(String depLoc, String arrLoc, String flightDate,
+                                   int choice) {
 
-		
-		Map<String, String> flList = new HashMap<String, String>();
-		try {
-			for (String obj : listPath) {
+        String newLine = "";
+        BufferedReader br = null;
+        int flag = 0;
+        int selCol = 5;
 
-				br = new BufferedReader(new FileReader(obj));
+        if (choice == 1)
+            selCol = 6;
+        else
+            selCol = 5;
 
-				while ((newLine = br.readLine()) != null) {
 
-					String[] comp1 = newLine.split(splitBy);
+        Map<String, String> flList = new HashMap<String, String>();
+        try {
+            for (String obj : listPath) {
 
-					if ((comp1[1].equalsIgnoreCase(depLoc)) && (comp1[2].equalsIgnoreCase(arrLoc))
-							&& (comp1[3].equalsIgnoreCase(flightDate))) {
-						String line1 = toString(comp1[0], comp1[1], comp1[2],
-								comp1[3], comp1[4], comp1[5], comp1[6]);
-						flList.put(comp1[selCol], line1);
-						flag=1;
-						
-					}
-				}
-			}
-		} catch (FileNotFoundException e) {
-			
-			e.printStackTrace();
-		} catch (IOException e) {
-			
-			e.printStackTrace();
-		}
+                br = new BufferedReader(new FileReader(obj));
 
-		if (flag == 1) {
-			finalList=sortByPreference(flList, selCol);
-			try {
-				br.close();
-			} catch (IOException e) {
-				
-				e.printStackTrace();
-			}
-			return finalList;
+                while ((newLine = br.readLine()) != null) {
 
-		} else {
-			finalList.add("Sorry, We dont have flight for your search!\nTry again!");
-			return finalList;
-		}
-	}
+                    String[] comp1 = newLine.split(STRING_COMMA);
 
-	private List<String> sortByPreference(Map<String, String> flList, int choice) {
-	
-		Set<String> keyset=flList.keySet();
-		List<Double> DuraList=new ArrayList<Double>();
-		List<Long> fareList=new ArrayList<Long>();
-		
-		for(Object cnt:keyset)
-		{
-			if(choice==5)
-			{
-				Double val=Double.parseDouble(toString(cnt));
-				DuraList.add(val);
-				
-			}
-			else
-			{
-				Long val=Long.parseLong(toString(cnt),10);
-				fareList.add(val);
-			}
-		}
+                    if ((comp1[1].equalsIgnoreCase(depLoc)) && (comp1[2].equalsIgnoreCase(arrLoc))
+                            && (comp1[3].equalsIgnoreCase(flightDate))) {
+                        String line1 = toString(comp1[0], comp1[1], comp1[2],
+                                comp1[3], comp1[4], comp1[5], comp1[6]);
+                        flList.put(comp1[selCol], line1);
+                        flag = 1;
 
-		if(choice==5)
-		{
-			Collections.sort(DuraList);
-			for(Object cnt:DuraList)
-			{
-				finalList.add(flList.get(cnt.toString()));
-				
-			}
-		}
-		else
-		{
-			Collections.sort(fareList);
-			for(Object cnt:fareList)
-			{
-				finalList.add(flList.get(cnt.toString()));
-				
-			}
-		}
-		return finalList;
-	}
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
 
-	private String toString(Object cnt) {
-		
-		return ""+cnt;
-	}
+            e.printStackTrace();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+        if (flag == 1) {
+            finalList = sortByPreference(flList, selCol);
+            try {
+                br.close();
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+            return finalList;
+
+        } else {
+            finalList.add("Sorry, We dont have flight for your search!\nTry again!");
+            return finalList;
+        }
+    }
+
+    private List<String> sortByPreference(Map<String, String> flList, int choice) {
+
+        Set<String> keyset = flList.keySet();
+        List<Double> DuraList = new ArrayList<Double>();
+        List<Long> fareList = new ArrayList<Long>();
+
+        for (Object cnt : keyset) {
+            if (choice == 5) {
+                Double val = Double.parseDouble(toString(cnt));
+                DuraList.add(val);
+
+            } else {
+                Long val = Long.parseLong(toString(cnt), 10);
+                fareList.add(val);
+            }
+        }
+
+        if (choice == 5) {
+            Collections.sort(DuraList);
+            for (Object cnt : DuraList) {
+                finalList.add(flList.get(cnt.toString()));
+
+            }
+        } else {
+            Collections.sort(fareList);
+            for (Object cnt : fareList) {
+                finalList.add(flList.get(cnt.toString()));
+
+            }
+        }
+        return finalList;
+    }
+
+    private String toString(Object cnt) {
+
+        return "" + cnt;
+    }
 }
