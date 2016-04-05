42b84b20c6ccf622ead6339a770b693c8b11e712
Rahul Shukla <rahul.shukla@synerzip.com>
2016-04-05 14:30:27 +0530

    removing client reference from backend code

--- src/com/FlightSearchEngineClient.java
+++ src/com/FlightSearchEngineClient.java
@@ -7,47 +7,45 @@ import java.util.List;
 import java.util.Scanner;
 
 public class FlightSearchEngineClient {
-	private static SearchFlight sf;
-	private static String header;
-	public static Scanner sc = new Scanner(System.in);
+    private static SearchFlight sf;
+    public static Scanner sc = new Scanner(System.in);
 
-	public FlightSearchEngineClient(String header) {
-		FlightSearchEngineClient.header = header;
-	}
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.out.println("Enter number of .csv fils:");
-		int totalFiles = sc.nextInt();
-		System.out.println("Enter path for .csv file:");
-		List<String> path = new ArrayList<>();
-		for (int cnt = 0; cnt < totalFiles; cnt++) {
-			path.add(sc.next());
-		}
 
-		sf = new SearchFlight(path, new HashMap<Integer, Comparator<Flight>>());
-		scanDetails();
-	}
+        System.out.println("Enter number of .csv fils:");
+        int totalFiles = sc.nextInt();
+        System.out.println("Enter path for .csv file:");
 
-	public static void scanDetails() {
+        List<String> path = new ArrayList<>();
+        for (int cnt = 0; cnt < totalFiles; cnt++) {
+            path.add(sc.next());
+        }
 
-		System.out.println("Enter Departure Location:");
-		String depLoc = sc.next();
-		System.out.println("Enter Arrival Location:");
-		String arrLoc = sc.next();
-		System.out.println("Enter Flight Date:");
-		String flightDate = sc.next();
-		System.out
-				.println("Enter\n 1 To sort According To Fare\n 2 To sort According To Fare-Duration");
-		int choice = sc.nextInt();
-		List<Flight> finalList = sf.getFlights(depLoc, arrLoc, flightDate,
-				choice);
-		sc.close();
+        sf = new SearchFlight(path, new HashMap<Integer, Comparator<Flight>>());
+        scanDetails();
+    }
 
-		System.out.println(header);
-		for (Flight obj : finalList) {
-			System.out.println(obj);
-		}
+    public static void scanDetails() {
 
-	}
+        System.out.println("Enter Departure Location:");
+        String depLoc = sc.next();
+        System.out.println("Enter Arrival Location:");
+        String arrLoc = sc.next();
+        System.out.println("Enter Flight Date:");
+        String flightDate = sc.next();
+        System.out
+                .println("Enter\n 1 To sort According To Fare\n 2 To sort According To Fare-Duration");
+        int choice = sc.nextInt();
+        List<Flight> finalList = sf.getFlights(depLoc, arrLoc, flightDate,
+                choice);
+        sc.close();
+
+        System.out.println(sf.getHeaders());
+        for (Flight obj : finalList) {
+            System.out.println(obj);
+        }
+
+    }
 }
--- src/com/SearchFlight.java
+++ src/com/SearchFlight.java
@@ -4,80 +4,81 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class SearchFlight {
-	public static final String STRING_COMMA = ",";
-	protected ArrayList<String> listPath = new ArrayList<String>();
+    public static final String STRING_COMMA = ",";
+    protected ArrayList<String> listPath = new ArrayList<String>();
 
-	private Map<Integer, Comparator<Flight>> sorter = new HashMap<>();
+    public String getHeaders() {
+        return headers;
+    }
 
-	public SearchFlight(List<String> path,
-			Map<Integer, Comparator<Flight>> extSorter) {
+    private String headers = "";
 
-		for (String p : path) {
-			listPath.add(p);
-		}
-		
-		sorter.put(2, new FareDurationComparator());
-		sorter.putAll(extSorter);
+    private Map<Integer, Comparator<Flight>> sorter = new HashMap<>();
 
-	}
+    public SearchFlight(List<String> path,
+                        Map<Integer, Comparator<Flight>> extSorter) {
 
-	public List<Flight> getFlights(String depLoc, String arrLoc,
-			String flightDate, int choice) {
+        for (String p : path) {
+            listPath.add(p);
+        }
 
-		String newLine = "";
-		BufferedReader br = null;
-		List<Flight> flList = new ArrayList<>();
+        sorter.put(2, new FareDurationComparator());
+        sorter.putAll(extSorter);
 
-		try {
-			for (String obj : listPath) {
-				br = new BufferedReader(new FileReader(obj));
-				new FlightSearchEngineClient(br.readLine());
-				while ((newLine = br.readLine()) != null) {
+    }
 
-					String[] readColumn = newLine.split(STRING_COMMA);
+    public List<Flight> getFlights(String depLoc, String arrLoc,
+                                   String flightDate, int choice) {
 
-					if ((readColumn[1].equalsIgnoreCase(depLoc))
-							&& (readColumn[2].equalsIgnoreCase(arrLoc))
-							&& (readColumn[3].equalsIgnoreCase(flightDate))) {
-						flList.add(new Flight(readColumn[0], readColumn[1],
-								readColumn[2], readColumn[3], Integer
-										.parseInt(readColumn[4]), Float
-										.parseFloat(readColumn[5]), Float
-										.parseFloat(readColumn[6])));
-					}
-				}
-				br.close();
-			}
-		} catch (FileNotFoundException e) {
+        String newLine = "";
+        BufferedReader br = null;
+        List<Flight> flList = new ArrayList<>();
 
-			e.printStackTrace();
-		} catch (IOException e) {
+        try {
+            for (String obj : listPath) {
+                br = new BufferedReader(new FileReader(obj));
+                headers = br.readLine();
+                while ((newLine = br.readLine()) != null) {
 
-			e.printStackTrace();
-		}
+                    String[] readColumn = newLine.split(STRING_COMMA);
 
-		return sortByPreference(flList, choice);
+                    if ((readColumn[1].equalsIgnoreCase(depLoc))
+                            && (readColumn[2].equalsIgnoreCase(arrLoc))
+                            && (readColumn[3].equalsIgnoreCase(flightDate))) {
+                        flList.add(new Flight(readColumn[0], readColumn[1],
+                                readColumn[2], readColumn[3], Integer
+                                .parseInt(readColumn[4]), Float
+                                .parseFloat(readColumn[5]), Float
+                                .parseFloat(readColumn[6])));
+                    }
+                }
+                br.close();
+            }
+        } catch (FileNotFoundException e) {
 
-	}
+            e.printStackTrace();
+        } catch (IOException e) {
 
-	private List<Flight> sortByPreference(List<Flight> flList, int choice) {
+            e.printStackTrace();
+        }
 
-		Comparator<Flight> flightComparator = sorter.get(choice);
-		if (flightComparator == null) {
-			Collections.sort(flList);
-			return flList;
-		}
-		Collections.sort(flList, flightComparator);
-		return flList;
+        return sortByPreference(flList, choice);
 
-	}
+    }
+
+    private List<Flight> sortByPreference(List<Flight> flList, int choice) {
+
+        Comparator<Flight> flightComparator = sorter.get(choice);
+        if (flightComparator == null) {
+            Collections.sort(flList);
+            return flList;
+        }
+        Collections.sort(flList, flightComparator);
+        return flList;
+
+    }
 
 }
